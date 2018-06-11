7f3ab1f82ab0644536f9b592ceddd249d81ef356
Chaopale Lamecarlate <Lamecarlate@users.noreply.github.com>
2018-06-11 11:10:35 +0200

    Encapsuler tout ça dans une IIFE (+ use strict)

--- octs.js
+++ octs.js
@@ -17,105 +17,109 @@
 // @require			https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // ==/UserScript==
 
-// Correction CSS barre de recherche
-$(".button--iconOnly").css({"padding":"0px","min-height":"28px","height":"28px"});
-$(".inputGroup__icon.icon-search").css({"margin-top":"6px"});
+(function($, document, undefined) {
+    'use strict';
 
-// Copie du fil d'ariane en bas du sujet
-$(".breadcrumb").clone().insertAfter($("section.comments"));
+    // Correction CSS barre de recherche
+    $(".button--iconOnly").css({"padding":"0px","min-height":"28px","height":"28px"});
+    $(".inputGroup__icon.icon-search").css({"margin-top":"6px"});
 
-// Bouton afficher/masquer les épinglés
-if( GM_getValue( "showPostIt" ) === undefined ) GM_setValue( "showPostIt" , true );
-if( window.location.href.indexOf( "forum" ) > 0 && window.location.href.indexOf( "sujet" ) <= 0 ) {
-    let messagesLists = document.querySelectorAll(".list");
-    messagesLists.forEach(function (list) {
-        if (list.querySelectorAll(".postit").length > 0) {
-            let button = document.createElement("button");
-            button.classList.add("btn", "btn-primary");
-            button.style.marginBottom = "20px";
-            button.addEventListener("click", function () {
+    // Copie du fil d'ariane en bas du sujet
+    $(".breadcrumb").clone().insertAfter($("section.comments"));
+
+    // Bouton afficher/masquer les épinglés
+    if( GM_getValue( "showPostIt" ) === undefined ) GM_setValue( "showPostIt" , true );
+    if( window.location.href.indexOf( "forum" ) > 0 && window.location.href.indexOf( "sujet" ) <= 0 ) {
+        let messagesLists = document.querySelectorAll(".list");
+        messagesLists.forEach(function (list) {
+            if (list.querySelectorAll(".postit").length > 0) {
+                let button = document.createElement("button");
+                button.classList.add("btn", "btn-primary");
+                button.style.marginBottom = "20px";
+                button.addEventListener("click", function () {
+                    toggleList(list, button);
+                });
+                list.parentNode.insertBefore(button, list);
                 toggleList(list, button);
-            });
-            list.parentNode.insertBefore(button, list);
-            toggleList(list, button);
+            }
+        });
+
+        function toggleList(list, button) {
+            let buttonText;
+            if (GM_getValue( "showPostIt" )) {
+                list.classList.remove("hidden");
+                buttonText = "Cacher les sujets épinglés";
+                GM_setValue( "showPostIt" , false );
+            } else {
+                list.classList.add("hidden");
+                buttonText = "Afficher les sujets épinglés";
+                GM_setValue( "showPostIt" , true );
+            }
+            button.innerHTML = buttonText;
+        }
+    }
+
+    // Bouton top
+    $("#mainContentWithHeader").append('<span title="Haut de la page" class="oc-mod-tooltip oc-mod-nav" id="oc-mod-top"><i class="icon-next"></i></span>');
+    if( $(window).scrollTop() < 100 ) {
+        $("#oc-mod-top").hide();
+    }
+    $("#oc-mod-top").click( () => {
+        $(window).scrollTop( 0 );
+    });
+
+    // Bouton bottom
+    $("#mainContentWithHeader").append('<span title="Bas de la page" class="oc-mod-tooltip oc-mod-nav" id="oc-mod-bottom"><i class="icon-next"></i></span>');
+    if( $(window).height()+$(window).scrollTop() > $(document).height()-250 ) {
+        $("#oc-mod-bottom").hide();
+    }
+    $("#oc-mod-bottom").click( () => {
+        $(window).scrollTop( $(document).height()-200 );
+    });
+
+    // Style bouton top/bottom
+    $(".icon-next").css({"display":"inline-block"});
+    $(".oc-mod-nav").css({
+        "cursor":"pointer",
+        "position":"fixed",
+        "right":"50px",
+        "background":"#4f8a03",
+        "border-radius":"5px",
+        "color":"#fff"
+    });
+    $("#oc-mod-top").css({
+        "padding":"11px 15px 15px 15px",
+        "top":"38%"
+    });
+    $("#oc-mod-top>i").css({"transform":"rotate(-90deg)"});
+    $("#oc-mod-bottom").css({
+        "padding":"17px 15px 9px 15px",
+        "bottom":"38%"
+    });
+    $("#oc-mod-bottom>i").css({"transform":"rotate(90deg)"});
+
+    // Gestion du scroll
+    $(window).scroll( () => {
+        if( $(window).scrollTop() > 100 ) {
+            $("#oc-mod-top").show();
+        } else {
+            $("#oc-mod-top").hide();
+        }
+        if( $(window).height()+$(window).scrollTop() < $(document).height()-250 ) {
+            $("#oc-mod-bottom").show();
+        } else {
+            $("#oc-mod-bottom").hide();
         }
     });
 
-    function toggleList(list, button) {
-        let buttonText;
-        if (GM_getValue( "showPostIt" )) {
-            list.classList.remove("hidden");
-            buttonText = "Cacher les sujets épinglés";
-            GM_setValue( "showPostIt" , false );
-        } else {
-            list.classList.add("hidden");
-            buttonText = "Afficher les sujets épinglés";
-            GM_setValue( "showPostIt" , true );
+    // Suppression des pubs
+    $(".adviceBanner").remove();
+
+    // Gestion des infobulles
+    $(".oc-mod-tooltip").tooltip( {
+        open: function( event, ui ) {
+            $(".ui-widget-shadow").css({"background":"#fff"});
+            $(".ui-widget-shadow").fadeTo(0,1);
         }
-        button.innerHTML = buttonText;
-    }
-}
-
-// Bouton top
-$("#mainContentWithHeader").append('<span title="Haut de la page" class="oc-mod-tooltip oc-mod-nav" id="oc-mod-top"><i class="icon-next"></i></span>');
-if( $(window).scrollTop() < 100 ) {
-	$("#oc-mod-top").hide();
-}
-$("#oc-mod-top").click( () => {
-	$(window).scrollTop( 0 );
-});
-
-// Bouton bottom
-$("#mainContentWithHeader").append('<span title="Bas de la page" class="oc-mod-tooltip oc-mod-nav" id="oc-mod-bottom"><i class="icon-next"></i></span>');
-if( $(window).height()+$(window).scrollTop() > $(document).height()-250 ) {
-	$("#oc-mod-bottom").hide();
-}
-$("#oc-mod-bottom").click( () => {
-	$(window).scrollTop( $(document).height()-200 );
-});
-
-// Style bouton top/bottom
-$(".icon-next").css({"display":"inline-block"});
-$(".oc-mod-nav").css({
-	"cursor":"pointer",
-	"position":"fixed",
-	"right":"50px",
-	"background":"#4f8a03",
-	"border-radius":"5px",
-	"color":"#fff"
-});
-$("#oc-mod-top").css({
-	"padding":"11px 15px 15px 15px",
-	"top":"38%"
-});
-$("#oc-mod-top>i").css({"transform":"rotate(-90deg)"});
-$("#oc-mod-bottom").css({
-	"padding":"17px 15px 9px 15px",
-	"bottom":"38%"
-});
-$("#oc-mod-bottom>i").css({"transform":"rotate(90deg)"});
-
-// Gestion du scroll
-$(window).scroll( () => {
-	if( $(window).scrollTop() > 100 ) {
-		$("#oc-mod-top").show();
-	} else {
-		$("#oc-mod-top").hide();
-	}
-	if( $(window).height()+$(window).scrollTop() < $(document).height()-250 ) {
-		$("#oc-mod-bottom").show();
-	} else {
-		$("#oc-mod-bottom").hide();
-	}
-});
-
-// Suppression des pubs
-$(".adviceBanner").remove();
-
-// Gestion des infobulles
-$(".oc-mod-tooltip").tooltip( {
-	open: function( event, ui ) {
-		$(".ui-widget-shadow").css({"background":"#fff"});
-   		$(".ui-widget-shadow").fadeTo(0,1);
-	}
-});
+    });
+})(window.jQuery, document);
