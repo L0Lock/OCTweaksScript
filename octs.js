14616ae4d5f395ea1c3eeae34977bd04699f02cb
benzouye <benzouye@hotmail.com>
2018-12-27 14:42:59 +0100

    v1.2.11

--- octs.js
+++ octs.js
@@ -6,7 +6,7 @@
 // @updateURL   		https://raw.githubusercontent.com/L0Lock/OCTweaksScript/master/octs.js
 // @downloadURL 		https://raw.githubusercontent.com/L0Lock/OCTweaksScript/master/octs.js
 // @include			*openclassrooms.com/*
-// @version			1.2.10
+// @version			1.2.11
 // @noframes
 // @grant			GM_getValue
 // @grant			GM_setValue
